e21435024f3999be06d30324e19b0dc33a5dd53a
IsthisLee <rjsgmldnwn@gmail.com>
2023-09-17 20:01:01 +0900

    chore: edit pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       instances: '0', // 클러스터 모드 사용 시 생성할 인스턴스 수(max/0은 CPU 수만큼 자동으로 생성)
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
       out_file: '/dev/null', // outlog 저장 경로 설정(/dev/null은 outlog 미저장)
-      error_file: '~/lawlow_logs/error', // errorlog 저장 경로 설정(/dev/null은 errorlog 미저장)
+      error_file: '~/sky-flower/error', // errorlog 저장 경로 설정(/dev/null은 errorlog 미저장)
       autorestart: true, // 프로그램이 멈추면 자동으로 재시작한다.
       max_memory_restart: '512M', // 프로그램의 메모리 크기가 일정 크기 이상이 되면 재시작한다.
       time: true, // log에 시간을 표시한다.
