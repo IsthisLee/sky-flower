02e0278f4ddbe7210f6553117ef029eacfd97420
IsthisLee <rjsgmldnwn@gmail.com>
2023-09-18 20:03:37 +0900

    feat: Add rate limit

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,24 @@ import { setupSwagger } from './common/swagger-setting';
 import { HttpStatus } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger:
-      process.env.NODE_ENV === 'development'
+      process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local'
         ? ['error', 'warn', 'log', 'verbose', 'debug']
         : ['error', 'warn', 'log'],
   });
 
   const configService = app.select(AppModule).get(ConfigService);
 
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   app.enableCors({
     origin: [
       configService.get('CLIENT_URL'),
