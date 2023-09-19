870958ee1c5c7846a478b63974ad112bc7725c53
IsthisLee <rjsgmldnwn@gmail.com>
2023-09-20 01:29:35 +0900

    feat: rateLimit 제거
    - 클러스터 모드의 경우, 여러 개의 인스턴스가 생성되므로 중앙 저장소(redis 등)를 통해 요청 IP를 기억해야 함.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { setupSwagger } from './common/swagger-setting';
 import { HttpStatus } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
-import rateLimit from 'express-rate-limit';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
@@ -17,16 +16,6 @@ async function bootstrap() {
   });
   const configService = app.select(AppModule).get(ConfigService);
 
-  // 클라이언트의 실제 IP 주소를 가져오기 위함
-  // (앱이 프록시 서버 뒤에 있는 경우 X-Forwarded-For 헤더에 클라이언트의 IP 주소가 포함되어 있음. 이 헤더를 신뢰한기 위한 설정)
-  // 리버스 프록시 뒤에 배포된 애플리케이션에서만 활성화. 그렇지 않으면, 악의적인 사용자가 위의 헤더를 조작하여 애플리케이션을 속일 수 있음.
-  app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
   app.enableCors({
     origin: [
       configService.get('CLIENT_URL'),
